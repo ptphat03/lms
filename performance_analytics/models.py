294a392d5f30b36702e51dc7a8b41867baec18fe
Trunk2004 <122500213+Trunk2004@users.noreply.github.com>
2024-10-16 12:31:03 +0700

    Add files via upload

--- performance_analytics/models.py
+++ performance_analytics/models.py
@@ -3,7 +3,7 @@ from course.models import Course
 from quiz.models import Quiz,StudentQuizAttempt
 from user.models import User
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,post_delete
 
 # Create your models here.
 class PerformanceAnalytics(models.Model):
@@ -15,12 +15,13 @@ class PerformanceAnalytics(models.Model):
 
     def update_performance(self):
         # quiz = PerformanceAnalytics.objects.filter(course = self.course.id)
-        quizzes = StudentQuizAttempt.objects.filter(user =self.user.id,quiz__course = self.course)
-        total_quizzes = quizzes.count()
+
+        completed_quizzes = StudentQuizAttempt.objects.filter(user =self.user.id,quiz__course = self.course)
+        total_quizzes = Quiz.objects.filter(course = self.course).count()
         if total_quizzes > 0:
-            total_score = quizzes.aggregate(models.Sum('score'))['score__sum'] or 0
+            total_score = completed_quizzes.aggregate(models.Sum('score'))['score__sum'] or 0
             self.average_score = round(total_score / total_quizzes,1)
-            completed_quizzes = quizzes.filter(score__gt=0).count()
+            completed_quizzes = completed_quizzes.filter(score__gt=0).count()
             self.completion_rate = round(completed_quizzes / total_quizzes,2) *100
         else:
             self.average_score = 0.0
@@ -31,9 +32,20 @@ class PerformanceAnalytics(models.Model):
         db_table = 'performance' 
 
 @receiver(post_save, sender=StudentQuizAttempt)
+@receiver(post_delete, sender=StudentQuizAttempt )
 def update_performance_analytics(sender, instance, **kwargs):
     try:
         performance = PerformanceAnalytics.objects.get( user = instance.user,course = instance.quiz.course)
     except PerformanceAnalytics.DoesNotExist:
         performance = PerformanceAnalytics( user= instance.user,course = instance.quiz.course)
+    performance.update_performance()
+
+
+@receiver(post_save, sender=Quiz)
+@receiver(post_delete, sender=Quiz)
+def update_completion_rate(sender, instance,created, **kwargs):
+    try:
+        performance = PerformanceAnalytics.objects.get( course = instance.course)
+    except PerformanceAnalytics.DoesNotExist:
+        performance = PerformanceAnalytics( course = instance.course)
     performance.update_performance()
