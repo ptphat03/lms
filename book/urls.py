0458638226c722d94b10bcb886d7b7e48cde395b
Khang-gif <Khangphse180736@fpt.edu.vn>
2024-10-22 08:46:03 +0700

    Delete book directory

--- book/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('search/', views.book_search_view, name='book_search'),
-    path('<str:book_id>/', views.book_detail_view, name='book_detail'),
-]
