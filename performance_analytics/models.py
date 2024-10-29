f8cb5837a0dc1a419a4a647cf1f6b6e857ec9b1f
Trunk2004 <122500213+Trunk2004@users.noreply.github.com>
2024-10-30 03:57:19 +0700

    Add files via upload

--- performance_analytics/models.py
+++ performance_analytics/models.py
@@ -1,36 +1,37 @@
 from django.db import models
-from course.models import Course
-from quiz.models import Quiz,StudentQuizAttempt
-from user.models import User
+from course.models import Course,Enrollment
+from assessments.models import Assessment, StudentAssessmentAttempt
+from user.models import User,Student
 from django.dispatch import receiver
 from django.db.models.signals import post_save,post_delete
 from django.db.models import OuterRef, Subquery,Max
+from django.contrib.auth.decorators import login_required
 # Create your models here.
 class PerformanceAnalytics(models.Model):
     id = models.AutoField(primary_key=True) 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     average_score = models.FloatField(default=0.0)
-    completion_rate = models.IntegerField(default=0.0)
+    completion_rate = models.FloatField(default=0.0)
 
     def update_performance(self):
-        # quiz = PerformanceAnalytics.objects.filter(course = self.course.id)
-        latest_attempt_dates = StudentQuizAttempt.objects.filter(
-            user=self.user, quiz = OuterRef('quiz')
-        ).values('quiz').annotate(latest_date=Max('attempt_date')).values('latest_date')
+        highest_score = StudentAssessmentAttempt.objects.filter(
+            user=self.user, assessment=OuterRef('assessment')
+        ).values('assessment').annotate(highest_score=Max('score_ass')).values('highest_score')
 
-        latest_attempts = StudentQuizAttempt.objects.filter(
+        highest_attempts = StudentAssessmentAttempt.objects.filter(
             user=self.user,
-            quiz__course=self.course,
-            attempt_date__in=Subquery(latest_attempt_dates)
+            assessment__course=self.course,
+            score_ass__in=Subquery(highest_score)
         )
         
-        total_quizzes = Quiz.objects.filter(course = self.course).count()
-        if total_quizzes > 0:
-            total_score = latest_attempts.aggregate(models.Sum('score'))['score__sum'] or 0
-            self.average_score = round(total_score / total_quizzes,3)
-            completed_quizzes = latest_attempts.filter(score__gt=0).count()
-            self.completion_rate = round(completed_quizzes / total_quizzes,2) *100
+        total_assessments = Assessment.objects.filter(course=self.course).count()
+        if total_assessments > 0:
+            completed_assessments = highest_attempts.filter().count()
+            print('count:',completed_assessments)
+            total_score = highest_attempts.aggregate(models.Sum('score_ass'))['score_ass__sum'] or 0
+            self.average_score = round(total_score / completed_assessments, 3)
+            self.completion_rate = round((completed_assessments / total_assessments) * 100, 2)
         else:
             self.average_score = 0.0
             self.completion_rate = 0.0
@@ -39,25 +40,47 @@ class PerformanceAnalytics(models.Model):
     class Meta:
         db_table = 'performance' 
 
-@receiver(post_save, sender=StudentQuizAttempt)
-@receiver(post_delete, sender=StudentQuizAttempt )
+@receiver(post_save, sender=StudentAssessmentAttempt)
+@receiver(post_delete, sender=StudentAssessmentAttempt)
 def update_performance_analytics(sender, instance, **kwargs):
     try:
-        performance = PerformanceAnalytics.objects.get( user = instance.user,course = instance.quiz.course)
+        performance = PerformanceAnalytics.objects.get(
+            user=instance.user,
+            course=instance.assessment.course 
+        )
+        performance.update_performance()
     except PerformanceAnalytics.DoesNotExist:
-        performance = PerformanceAnalytics( user= instance.user,course = instance.quiz.course)
-    performance.update_performance()
+        pass
 
 
-@receiver(post_save, sender=Quiz)
-@receiver(post_delete, sender=Quiz)
-def update_completion_rate(sender, instance, **kwargs):
-    
-        students = User.objects.filter(profile__role__role_name='student', enrollment__course=instance.course)
+
+@receiver(post_save, sender=Assessment)
+@receiver(post_delete, sender=Assessment)
+def update_performance_analytics(sender, instance, **kwargs):
+        students = User.objects.filter(id__in=StudentAssessmentAttempt.objects.filter(assessment__course=instance.course).values('user'))
         for student in students:
-            try:
-                performance = PerformanceAnalytics.objects.get( user= student,course = instance.course)
-            except PerformanceAnalytics.DoesNotExist:
-                performance = PerformanceAnalytics( user = student,course = instance.course)
-            performance.update_performance()
-    
+            performance = PerformanceAnalytics.objects.get(
+                user=student,
+                course=instance.course
+            )
+        performance.update_performance()
+
+
+@receiver(post_save, sender=Enrollment)
+def create_performance(sender, instance, created, **kwargs):
+    if created:
+        PerformanceAnalytics.objects.create(
+            user=instance.student,
+            course=instance.course
+        )
+
+@receiver(post_delete, sender=Enrollment)
+def delete_performance(sender, instance, **kwargs):
+    try:
+        performance = PerformanceAnalytics.objects.get(
+            user=instance.student,
+            course=instance.course
+        )
+        performance.delete()
+    except PerformanceAnalytics.DoesNotExist:
+        pass
